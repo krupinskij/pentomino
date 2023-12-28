c9e07d60be41a9f1089770337281e9293a7f992a
krupinskij <jk.krupinski@gmail.com>
2023-12-29 00:55:29 +0100

    Add solutions info

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub mod config;
 pub fn run(config: Config) {
     let board = Board::new(config.height, config.width);
     let mut blocks: Vec<Block> = Vec::new();
+    let mut solutions: u32 = 0;
 
     config.blocks.chars().for_each(|c| {
         if let Some(block) = Block::build(c) {
@@ -16,14 +17,16 @@ pub fn run(config: Config) {
         };
     });
 
-    match_blocks(board, blocks)
+    match_blocks(board, blocks, &mut solutions);
+
+    println!("{} solution(s) found", solutions)
 }
 
-fn match_blocks(mut board: Board, blocks: Vec<Block>) {
-    match_blocks_rec(&mut board, &blocks, 0)
+fn match_blocks(mut board: Board, blocks: Vec<Block>, solutions: &mut u32) {
+    match_blocks_rec(&mut board, &blocks, 0, solutions)
 }
 
-fn match_blocks_rec(board: &mut Board, blocks: &Vec<Block>, i: usize) {
+fn match_blocks_rec(board: &mut Board, blocks: &Vec<Block>, i: usize, solutions: &mut u32) {
     if let Some(block) = blocks.get(i) {
         for variant in block.variants.iter() {
             for x in 0..board.width {
@@ -31,7 +34,7 @@ fn match_blocks_rec(board: &mut Board, blocks: &Vec<Block>, i: usize) {
                     let is_added = board.try_add_block(x, y, &variant, block);
 
                     if is_added {
-                        match_blocks_rec(board, &blocks, i + 1);
+                        match_blocks_rec(board, &blocks, i + 1, solutions);
 
                         board.clear_block(x, y, &variant)
                     }
@@ -39,6 +42,7 @@ fn match_blocks_rec(board: &mut Board, blocks: &Vec<Block>, i: usize) {
             }
         }
     } else {
-        println!("{}", board)
+        println!("{}", board);
+        *solutions += 1;
     }
 }
